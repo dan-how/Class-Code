0fe342e158c5f003808a06c20aad0163bf13f4e8
dan-how <58979902+dan-how@users.noreply.github.com>
2020-01-10 16:45:37 +0000

    Add files via upload

---
+++ Menu test.py
@@ -0,0 +1,32 @@
+print("Menu")
+print("1 - Add")
+print("2 - Amend")
+print("3 - Delete")
+print("4 - Display")
+
+menu_option = int(input("Enter options: "))
+
+"""
+if menu_option == 1:
+    print("Option 1 selected")
+if menu_option == 2:
+    print("Option 2 selected")
+if menu_option == 3:
+    print("Option 3 selected")
+if menu_option == 4:
+    print("Option 4 selected")
+else:
+    print("Invalid option")
+
+"""
+
+if menu_option == 1:
+    print("Option 1 selected")
+    if menu_option == 2:
+        print("Option 2 selected")
+        if menu_option == 3:
+            print("Option 3 selected")
+            if menu_option == 4:
+                print("Option 4 selected")
+else:
+    print("Invalid option")
